92426d6e2cde1a1750e79e6d524fbef65a412055
Anton Bondar <direx187@gmail.com>
2023-03-20 10:25:53 +0200

    forecast

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=07527b2eec7fabe9f6abacccea305013`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=07527b2eec7fabe9f6abacccea305013`;
 
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=07527b2eec7fabe9f6abacccea305013`;
   const handleSearch = (event) => {
     if (event.key === "Enter") {
       axios.get(url).then((response) => {
@@ -16,29 +17,6 @@ function App() {
       setLocation("");
     }
   };
-  
-  function getBackground() {
-    const now = new Date();
-    const hour = now.getHours();
-    const isDaytime = hour >= 6 && hour < 18;
-  
-    let backgroundClass = "day"; // Default background class for daytime
-  
-    if (!isDaytime) {
-      backgroundClass = "night"; // Use nighttime background class
-    }
-  
-    // Check temperature range to set background image
-    if (this.state.temp >= 30) {
-      backgroundClass += " hot";
-    } else if (this.state.temp < 10) {
-      backgroundClass += " cold";
-    } else {
-      backgroundClass += " normal";
-    }
-  
-    return backgroundClass;
-  }
 
   return (
     <div className="app">
@@ -54,30 +32,150 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{data.name}</p>
+            {data.city ? <p>{data.city.name}</p> : null}
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp} F</h1> : null}
+            {data.list ? <h1>{data.list[0].main.temp} F</h1> : null}
           </div>
           <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.list ? <p>{data.list[0].weather[0].main}</p> : null}
           </div>
         </div>
 
         <div className="bottom">
           <div className="feels">
-            {data.main ? <p className="bold">{data.main.feels_like}</p> : null}
+            {data.list ? (
+              <p className="bold">{data.list[0].main.feels_like}</p>
+            ) : null}
             <p>Feels like</p>
           </div>
           <div className="humidity">
-            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            {data.list ? (
+              <p className="bold">{data.list[0].main.humidity}%</p>
+            ) : null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            {data.wind ? <p className="bold">{data.wind.speed} MPH</p> : null}
+            {data.list ? (
+              <p className="bold">{data.list[0].wind.speed} MPH</p>
+            ) : null}
             <p>Wind speed</p>
           </div>
         </div>
+        {/* <div className="prediction">
+          {data.list &&
+            data.list.slice(5, 6).map((item, index) => (
+              <div className="forecast" key={index}>
+                <p>
+                  {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+                <p>{item.weather[0].main}</p>
+                <p>{item.main.temp.toFixed()} F</p>
+              </div>
+            ))}
+        </div>
+        <div className="prediction">
+          {data.list &&
+            data.list.slice(13, 14).map((item, index) => (
+              <div className="forecast" key={index}>
+                <p>
+                  {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+                <p>{item.weather[0].main}</p>
+                <p>{item.main.temp.toFixed()} F</p>
+              </div>
+            ))}
+        </div>
+        <div className="prediction">
+          {data.list &&
+            data.list.slice(22, 23).map((item, index) => (
+              <div className="forecast" key={index}>
+                <p>
+                  {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+                <p>{item.weather[0].main}</p>
+                <p>{item.main.temp.toFixed()} F</p>
+              </div>
+            ))}
+        </div> */}
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              {data.list &&
+                data.list.slice(5, 6).map((item, index) => (
+                  <div className="forecast" key={index}>
+                    <p>
+                      {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                        weekday: "long",
+                        // year: "numeric",
+                        // month: "long",
+                        // day: "numeric",
+                      })}
+                    </p>
+                    <p>{item.weather[0].main}</p>
+                    <p>{item.main.temp.toFixed()} F</p>
+                  </div>
+                ))}
+            </div>
+            
+          </div>
+          <div className="column">
+            <div className="card">
+              {data.list &&
+                data.list.slice(13, 14).map((item, index) => (
+                  <div className="forecast" key={index}>
+                    <p>
+                      {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                        weekday: "long",
+                        // year: "numeric",
+                        // month: "long",
+                        // day: "numeric",
+                      })}
+                    </p>
+                    <p>{item.weather[0].main}</p>
+                    <p>{item.main.temp.toFixed()} F</p>
+                  </div>
+                ))}
+            </div>
+            
+          </div>
+          <div className="column">
+            <div className="card">
+              {data.list &&
+                data.list.slice(23, 24).map((item, index) => (
+                  <div className="forecast" key={index}>
+                    <p>
+                      {new Date(item.dt * 1000).toLocaleDateString("en-US", {
+                        weekday: "long",
+                        // year: "numeric",
+                        // month: "long",
+                        // day: "numeric",
+                      })}
+                    </p>
+                    <p>{item.weather[0].main}</p>
+                    <p>{item.main.temp.toFixed()} F</p>
+                  </div>
+                ))}
+            </div>
+            
+          </div>
+        </div>
+        
       </div>
     </div>
   );
